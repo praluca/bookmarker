91476f50867736e260fa23969fe4b8c9a195c013
Popa Raluca-Nicoleta <poparaluca98@yahoo.com>
2024-11-27 13:24:52 +0200

    add angular material

--- bookmark/src/app/modules/main-view/main-view/main-view.module.ts
+++ bookmark/src/app/modules/main-view/main-view/main-view.module.ts
@@ -4,6 +4,9 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BookmarksContentComponent } from '../../components/bookmarks-content/bookmarks-content.component';
 import { BookmarksListComponent } from '../../components/bookmarks-content/bookmarks-list/bookmarks-list.component';
+import { AddBookmarkDialogComponent } from 'src/app/shared/components/add-bookmark-dialog/add-bookmark-dialog.component';
+import { DeleteBookmarkDialogComponent } from 'src/app/shared/components/delete-bookmark-dialog/delete-bookmark-dialog.component';
+import { MaterialSharedModule } from 'src/app/shared/material-shared.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,7 @@ import { BookmarksListComponent } from '../../components/bookmarks-content/bookm
     BookmarksContentComponent,
     BookmarksListComponent,
   ],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, MaterialSharedModule],
   exports: [HeaderComponent, BookmarksContentComponent],
 })
 export class MainViewModule {}
